d39a6fbb0690ccbcb5d6c4509cb3f9b1f3a5bd5a
eisrkov <vsvktor@gmail.com>
2022-05-25 15:46:18 +0300

    Fragment

--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemFragment.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemFragment.kt
@@ -10,30 +10,30 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.databinding.ShopItemFragmentBinding
 import com.example.shoppinglist.domain.ShopItem
+import kotlin.concurrent.fixedRateTimer
 
-class ShopItemFragment(
-    private val screenMode: String = MODE_UNKNOWN,
-    private val shopItemId: Int = ShopItem.DEFAULT_ID
-) : Fragment() {
+class ShopItemFragment() : Fragment() {
     lateinit var binding: ShopItemFragmentBinding
     lateinit var viewModel: ShopItemViewModel
+    private var screenMode = MODE_UNKNOWN
+    private var shopItemId = ShopItem.DEFAULT_ID
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parsParam()
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = ShopItemFragmentBinding.inflate(layoutInflater, container, false)
         return binding.root
 
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        parsParam()
         viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
         setViewModelFun()
         addChangeTextListener()
         selectMod()
-
     }
-
     private fun addChangeTextListener() = with(binding) {
         etName.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -105,12 +105,20 @@ class ShopItemFragment(
     }
 
     private fun parsParam() {
-        if (screenMode != MODE_ADD && screenMode != MODE_EDIT) {
+        val args = requireArguments()
+        if (!args.containsKey(EXTRA_SCREEN_MODE)) {
+            throw RuntimeException("Параметр скрин мода отсутствует")
+        }
+        val mode = args.getString(EXTRA_SCREEN_MODE)
+        if (mode != MODE_EDIT && mode != MODE_ADD) {
             throw RuntimeException("Неизвестный мод экрана")
         }
-
-        if (screenMode == MODE_EDIT && shopItemId < 0) {
-            throw RuntimeException("ИД покупки не найдено")
+        screenMode = mode
+        if (mode == MODE_EDIT) {
+            if (!args.containsKey(EXTRA_SHOP_ITEM_ID)) {
+                throw RuntimeException("ИД покупки не найдено")
+            }
+            shopItemId = args.getInt(EXTRA_SHOP_ITEM_ID, ShopItem.DEFAULT_ID)
         }
 
     }
@@ -119,14 +127,25 @@ class ShopItemFragment(
         private const val MODE_ADD = "mode_add"
         private const val MODE_EDIT = "mode_edit"
         private const val MODE_UNKNOWN = ""
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
 
 
         fun getInstanceAddFragment(): ShopItemFragment {
-            return ShopItemFragment(MODE_ADD)
+            return ShopItemFragment().apply {
+                arguments = Bundle().apply {
+                    putString(EXTRA_SCREEN_MODE, MODE_ADD)
+                }
+            }
         }
 
         fun getInstanceEditFragment(shopItemId: Int): ShopItemFragment {
-            return ShopItemFragment(MODE_EDIT, shopItemId)
+            return ShopItemFragment().apply {
+                arguments = Bundle().apply {
+                    putString(EXTRA_SCREEN_MODE, MODE_EDIT)
+                    putInt(EXTRA_SHOP_ITEM_ID, shopItemId)
+                }
+            }
         }
     }
 }
